2c1a9f3ea260274d4b721d9ae686e054f438a184
Anthony Stanix <anthony.stanix@gmail.com>
2024-03-29 11:44:56 +0100

    add predictions in webapp

--- Home.py
+++ Home.py
@@ -2,6 +2,7 @@ from functions import *
 import streamlit as st
 import json
 import pickle
+import datetime
 
 st.set_page_config(page_title="Weather App Prévision", page_icon="🌦️", layout="wide")
 
@@ -13,5 +14,27 @@ st.sidebar.title("Weather App Prévision 🌦️")
 st.sidebar.write("Projet Deep Learning, TimeSeries")
 st.sidebar.link_button("📄 Lien vers la consigne", "https://meteo.data.gouv.fr/hackathon",)
 
-st.write("## Prédictions")
-st.write(temperature_prediction())    
+# Dates
+dates = []
+index = []
+today = '03/29/24'
+today = datetime.datetime.strptime(today, "%m/%d/%y")
+dates.append(today)
+for i in range(7):
+    today = today + datetime.timedelta(days=1)
+    dates.append(today)
+    index.append(i)
+st.write(dates)
+
+# Prédictions
+st.write("## Prédictions") 
+col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+cols = [col1, col2, col3, col4, col5, col6, col7]
+# st.write("<table> <tr> <th>Température</th> <th>Humidité</th> <th>Pression</th> <th>Vent moyen</th> </tr> <tr> <td>12.0</td> <td>80.0</td> <td>1000.0</td> <td>30.0</td> </tr> <tr> <td>12.0</td> <td>80.0</td> <td>1000.0</td> <td>30.0</td> </tr> <tr> <td>12.0</td> <td>80.0</td> <td>1000.0</td> <td>30.0</td> </tr> <tr> <td>12.0</td> <td>80.0</td> <td>1000.0</td> <td>30.0</td> </tr> <tr> <td>12.0</td> <td>80.0</td> <td>1000.0</td> <td>30.0</td> </tr> <tr> <td>12.0</td> <td>80.0</td> <td>1000.0</td> <td>30.0</td> </tr> <tr> <td>12.0</td> <td>80.0</td> <td>1000.0</td> <td>30.0</td> </tr> </table>", unsafe_allow_html=True)
+for index, dates, temperature_prediction, humidite_prediction, pression_prediction, vent_moyen_prediction in zip(index, dates, temperature_prediction(), humidite_prediction(), pression_prediction(), vent_moyen_prediction()):
+    with cols[index]:
+        st.write(dates)
+        st.write(temperature_prediction)
+        st.write(index, dates, temperature_prediction, humidite_prediction, pression_prediction, vent_moyen_prediction)
+        src, width, height = model_classification(temperature_prediction, pression_prediction, humidite_prediction, vent_moyen_prediction)
+        pred = st.components.v1.iframe(src, width, height, scrolling=False)
--- functions.py
+++ functions.py
@@ -1,9 +1,22 @@
 import pickle
 import numpy as np
+import datetime
 
 def temperature_prediction():
     temperature = pickle.load(open("./models/temperature_model.pickle", "rb"))
     return temperature.forecast(7)
+
+def humidite_prediction():
+    humidite = pickle.load(open("./models/humidite_model.pickle", "rb"))
+    return humidite.forecast(7)
+
+def pression_prediction():
+    pression = pickle.load(open("./models/pression_model.pickle", "rb"))
+    return pression.forecast(7)
+
+def vent_moyen_prediction():
+    vent_moyen = pickle.load(open("./models/vent_moyen_model.pickle", "rb"))
+    return vent_moyen.forecast(7)
 
 def model_classification(temperature, pression, humidite, vent_moyen):
     classif = pickle.load(open("./models/classification_model.pickle", "rb"))
@@ -14,9 +27,18 @@ def model_classification(temperature, pression, humidite, vent_moyen):
 
     # Afficher le résultat de la prédiction
     if prediction[0] == 1:
+        # il pleut
+        if vent_moyen > 20:
+            show = ["https://giphy.com/embed/77di5IQoTBRyGzDxgE", 480, 320]
+            return show
+        
         show = ["https://giphy.com/embed/W0c3xcZ3F1d0EYYb0f", 480, 400]
         return show   
     
+    # il ne pleut pas
+    if vent_moyen > 20:
+        show = ["https://giphy.com/embed/G5n8sqIOxBqow", 480, 337]
+        return show
+    
     show = ["https://giphy.com/embed/qhO10687JYjl4CsPyE", 480, 282]
     return show
-
